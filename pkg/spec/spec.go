d0712d1a1078138406fb4b5284f98ced25ab6b9a
Barak Michener <me@barakmich.com>
2016-11-22 15:54:33 -0800

    Add Spec implementation

---
+++ pkg/spec/spec.go
@@ -0,0 +1,67 @@
+// Copyright 2016 The quartermaster Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package spec
+
+import (
+	"k8s.io/client-go/pkg/api/unversioned"
+	"k8s.io/client-go/pkg/api/v1"
+)
+
+// StorageNode defines a single instance of available storage on a
+// node and the appropriate options to apply to it to make it available
+// to the cluster.
+type StorageNode struct {
+	unversioned.TypeMeta `json:",inline"`
+	v1.ObjectMeta        `json:"metadata,omitempty"`
+	Spec                 StorageNodeSpec `json:"spec"`
+}
+
+// StorageNodeSpec holds specification parameters for a StorageNode.
+type StorageNodeSpec struct {
+	Type           string                     `json:"type"` // Storage Implementation to use atop this storage.
+	NodeSelector   *unversioned.LabelSelector `json:"nodeSelector"`
+	StorageNetwork *StorageNodeNetwork        `json:"storageNetwork"`
+	Devices        []string                   `json:"devices"`     // Raw block devices available on the StorageNode to be used for storage.
+	Directories    []string                   `json:"directories"` // Directory-based storage available on the StorageNode to be used for storage.
+	GlusterFS      *GlusterStorageNode        `json:"glusterfs"`
+	Torus          *TorusStorageNode          `json:"torus"`
+	NFS            *NFSStorageNode            `json:"nfs"`
+}
+
+// StorageNodeNetwork specifies which network interfaces the StorageNode should
+// use for data transport, which may be separate from it's Kubernetes-accessible
+// IP.
+type StorageNodeNetwork struct {
+	IPs []string `json:"ips"`
+}
+
+// GlusterStorageNode defines the specifics of how this Gluster instance should be instantiated.
+type GlusterStorageNode struct {
+	Cluster string `json:"cluster"` // Cluster ID this node should belong to.
+	Zone    string `json:"zone"`    // Zone ID this node belongs to. If missing, Zone 1 will be assumed.
+}
+
+// TorusStorageNode defines the specifics of how this Gluster instance should be instantiated.
+type TorusStorageNode struct {
+	MonitorPort string `json:"monitorPort"`
+}
+
+// NFSStorageNode defines the specifics of how this Gluster instance should be instantiated.
+type NFSStorageNode struct {
+	// TODO(barakmich): More NFS options as per `man nfs` or `man exports`.
+	ReadOnly     bool `json:"readOnly"`     // This node exports NFS volumes ReadOnly.
+	SubtreeCheck bool `json:"subtreeCheck"` // Enable mount subtree checking on the host.
+	NoRootSquash bool `json:"noRootSquash"` // Disable root squashing, mapping UID 0 in the client to UID 0 on the host.
+}
